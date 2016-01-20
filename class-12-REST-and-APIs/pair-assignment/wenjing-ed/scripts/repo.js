c6cac39ea721bd30e0aa6406867efcf5506e994c
cacizi41 <qiangwenjing@gmail.com>
2016-01-20 08:51:16 -0800

    to pull upstream'

--- class-12-REST-and-APIs/pair-assignment/wenjing-ed/scripts/repo.js
+++ class-12-REST-and-APIs/pair-assignment/wenjing-ed/scripts/repo.js
@@ -7,14 +7,18 @@
     // TODO: How would you like to fetch your repos? Don't forget to call the callback.
   //fetch repo with ajax -1st requirement
   //write a success function to do something -2nd requirement
-  //var qs = '?/per_page=.....'
-  //$.get('/github/users/cacizi41/repos'+ qs'')
-  // .done(funciton(data, message, xhr){
-  //     console.log('my-data' + data);
-  //    repos.all = data;
-  // })
-  // .done(callback)
-  // };
+  var qs = '?/per_page=5&sort=updated'
+
+  $.ajax({
+   url: 'https://api.github.com/users/cacizi41/repos' + qs,
+   type: 'GET',
+   headers: { 'Authorization': 'token ' + githubToken },
+   success: function(data, message, xhr) {
+     repos.all = data
+     console.log(data);
+   }
+   .done(callback);
+ });
 
   // DONE: Model method that filters the full collection for repos with a particular attribute.
   // You could use this to filter all repos that have a non-zero `forks_count`, `stargazers_count`, or `watchers_count`.
